465742430401f07905134911431605b452113c3c
theRightHoopRod <unknown.rdm@gmail.com>
2024-10-29 21:39:10 -0600

    se le dió formato

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.indigo,
-        title: const Center(
-          child: Text(
-            'Cow Info',
-            style: TextStyle(color: Colors.white),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.indigo,
+          title: const Center(
+            child: Text(
+              'Cow Info',
+              style: TextStyle(color: Colors.white),
+            ),
           ),
         ),
+        body: const Home(),
       ),
-      body: const Home(),
     ),
-  ));
+  );
 }
